5223e300ffd91e3e70144940053c746d643e0df2
Mikhail Stolbov <mstolbov@gmail.com>
2012-12-11 18:54:45 +0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Castle::Application.config.secret_token = 'af7a455dcb08e1b00ccf9aaa597f4dc6fad7971ec2d3af84c256e8b9801fcca047b4e02f59adbe8c3711f2815b05a2e828e471fa1f387e4998578362060816be'
