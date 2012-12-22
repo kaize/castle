3dd3a915adc187ea21089216afdd4fb2417f5b14
Plugin73 <laplugin73@gmail.com>
2012-12-22 22:17:56 +0400

    fix link group to union

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ActiveRecord::Base
   attr_accessible :name, :union_id
 
-  belongs_to :union, inverse_of: :groups
+  belongs_to :union, inverse_of: :group
   has_many :schedules
 
   validates :name, presence: true
