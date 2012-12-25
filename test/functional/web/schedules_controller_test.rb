2b3cc86f15ac7ddcc5cd67d97e1472f7cdf5185b
Plugin73 <laplugin73@gmail.com>
2012-12-25 17:11:54 +0400

    fix tests

--- test/functional/web/schedules_controller_test.rb
+++ test/functional/web/schedules_controller_test.rb
@@ -1,13 +1,18 @@
 require 'test_helper'
 
 class Web::SchedulesControllerTest < ActionController::TestCase
+  def setup
+    @group = create :group
+    @params = {id: @group.id}
+  end
+
   test "should get index" do
     get :index
     assert_response :success
   end
 
   test "should get show" do
-    get :show
+    get :show, @params
     assert_response :success
   end
 
