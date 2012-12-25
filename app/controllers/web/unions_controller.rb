2b22f8ff4d1a19beab30737866a403df29a726c5
Plugin73 <laplugin73@gmail.com>
2012-12-25 10:40:07 +0400

    fix breadcrumbs path

--- app/controllers/web/unions_controller.rb
+++ app/controllers/web/unions_controller.rb
@@ -2,7 +2,7 @@ class Web::UnionsController < Web::ApplicationController
   def show
     @category = Category.find(params[:category_id])
     @union = @category.unions.find(params[:id])
-    add_breadcrumb @category.name, :category_path
+    add_breadcrumb @category.name, category_path(@category)
     add_breadcrumb @union.name, category_union_path(@category, @union)
   end
 end
