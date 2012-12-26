3e3f10d9d3a95c97525a63d9ad32c848b9e6bf3a
Plugin73 <laplugin73@gmail.com>
2012-12-26 17:28:36 +0400

    fix test

--- test/functional/web/categories_controller_test.rb
+++ test/functional/web/categories_controller_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
 class Web::CategoriesControllerTest < ActionController::TestCase
-  test "should get show" do
-    get :show
-    assert_response :success
+  def setup
+    @category = create :category
+    @params = {id: @category.id}
   end
 
+  test "should get show" do
+    get :show, @params
+    assert_response :success
+  end
 end
