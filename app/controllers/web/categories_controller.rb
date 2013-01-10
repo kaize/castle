ae02a8bf1f881e88c650d4634ff7f699d7b12e78
Plugin73 <laplugin73@gmail.com>
2013-01-10 15:50:07 +0400

    fix controllers hierarchy

--- app/controllers/web/categories_controller.rb
+++ app/controllers/web/categories_controller.rb
@@ -1,4 +1,10 @@
 class Web::CategoriesController < Web::ApplicationController
+
+  def index
+    add_breadcrumb :index, :categories_path
+    @category = Category.all
+  end
+
   def show
     @category = Category.find(params[:id])
     add_breadcrumb @category.name, :category_path
