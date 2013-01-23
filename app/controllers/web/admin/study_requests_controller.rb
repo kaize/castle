fce54c84d4464193e4a02eac4c25f09b03e06d87
login73ul <login73ul@gmail.com>
2013-01-23 16:17:50 +0400

    fixed flash for study requests

--- app/controllers/web/admin/study_requests_controller.rb
+++ app/controllers/web/admin/study_requests_controller.rb
@@ -7,7 +7,19 @@ class Web::Admin::StudyRequestsController < Web::Admin::ApplicationController
   def show
     @study_requests = StudyRequest.find(params[:id])
   end
+  
+  def create
+    @study_request = StudyRequest.new params[:study_request]
 
+    if @study_request.save
+      flash_success
+      redirect_to admin_study_requests_path
+    else
+      flash_error
+      render action: :new
+    end
+  end
+  
   def destroy
     @study_request = StudyRequest.find(params[:id])
     @study_request.destroy
