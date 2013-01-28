cef81e2caf211acd650ebc95083f33d2ef8663b3
login73ul <login73ul@gmail.com>
2013-01-28 10:07:35 +0400

    fix_nil_state_for_pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130125121942) do
+ActiveRecord::Schema.define(:version => 20130128053243) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
