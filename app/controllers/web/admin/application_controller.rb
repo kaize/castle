09a995c88bc7f11c76274842f41f63b28e2c2565
login73ul <login73ul@gmail.com>
2013-02-04 11:32:08 +0400

    helper for title

--- app/controllers/web/admin/application_controller.rb
+++ app/controllers/web/admin/application_controller.rb
@@ -1,2 +1,16 @@
 class Web::Admin::ApplicationController < Web::ApplicationController
+  helper_method :title_head
+
+  before_filter do
+    title_head
+  end
+
+  private
+
+  def title_head
+    scope = []
+    scope << params[:controller].split('/')
+    scope << params[:action] << [:title]
+    t(:scope => scope)  
+  end
 end
