1bf4a477a189838c65ce1d052a8dbe01510d5924
login73ul <login73ul@gmail.com>
2013-02-08 17:49:57 +0400

    fixes 58

--- app/models/group/schedule.rb
+++ app/models/group/schedule.rb
@@ -1,10 +1,10 @@
 class Group::Schedule < ActiveRecord::Base
-  attr_accessible :finish_at, :group_id, :start_at, :date, :classroom_id
+  attr_accessible :finish_at, :group_id, :start_at, :week, :classroom_id
 
   belongs_to :group, inverse_of: :schedules
   belongs_to :classroom
   
-  validates :date, presence: true
+  validates :week, presence: true
   validates :finish_at, presence: true
   validates :start_at, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130205130919) do
+ActiveRecord::Schema.define(:version => 20130208132535) do
 
   create_table "activities", :force => true do |t|
     t.string   "name"
@@ -80,7 +80,7 @@ ActiveRecord::Schema.define(:version => 20130205130919) do
     t.integer  "group_id"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
-    t.date     "date"
+    t.string   "week"
     t.integer  "classroom_id"
   end
 
