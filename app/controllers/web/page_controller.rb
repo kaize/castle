c23e4510c396d69b0f2eeb93ac8c118d5f4f73bc
Denis Starshenkov <starshenkov@bk.ru>
2013-02-21 12:55:28 +0000

    changed pages controller, added title for head

--- app/controllers/web/page_controller.rb
+++ app/controllers/web/page_controller.rb
@@ -1,7 +1,7 @@
 class Web::PageController < Web::ApplicationController
   def show
-    @page = Page.find_by_uri params[:id]
+    @page = Page.find_by_uri! params[:id]
     add_breadcrumb @page.title, page_path
-
+    title @page
   end
 end
