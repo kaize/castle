f63f2963fd6923c05530633a0a7a42ae3bd81dea
login73ul <login73ul@gmail.com>
2013-02-27 11:07:00 +0400

    add static menu for activity

--- app/controllers/web/activities/application_controller.rb
+++ app/controllers/web/activities/application_controller.rb
@@ -1,8 +1,6 @@
 class Web::Activities::ApplicationController < Web::ApplicationController
   helper_method :resource_activity
   
-  add_breadcrumb :home, :root_path
-  
   protected
 
     def resource_activity
--- app/controllers/web/activities/page_activities_controller.rb
+++ app/controllers/web/activities/page_activities_controller.rb
@@ -2,11 +2,14 @@ class Web::Activities::PageActivitiesController < Web::Activities::ApplicationCo
   def show
     @page_activity = resource_activity.page_activities.find(params[:id])
     
-    add_breadcrumb resource_activity.name, activity_path(resource_activity)
+    add_breadcrumb resource_activity.name, activity_page_activities_path(resource_activity)
     add_breadcrumb @page_activity.name, activity_page_activity_path(resource_activity, @page_activity)
   end
 
   def index
+    add_breadcrumb :home, :root_path
+    add_breadcrumb resource_activity.name, activity_path(resource_activity)
+    
     @page_activities = resource_activity.page_activities
   end
 end
