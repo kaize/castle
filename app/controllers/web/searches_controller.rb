6cc6df988081e192990f25937b6664ff9bf83f72
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2013-02-28 15:40:34 +0400

    add breadcrumb for search page

--- app/controllers/web/searches_controller.rb
+++ app/controllers/web/searches_controller.rb
@@ -1,5 +1,6 @@
 class Web::SearchesController < Web::ApplicationController
   def show
+    add_breadcrumb :search
     @term = params[:term]
     @results = PgSearcher.search(@term).page(params[:page]).per(20)
   end
