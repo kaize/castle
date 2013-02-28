fb748960bc3a5bf84e845862bd23b14ea33f966a
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2013-02-28 17:38:26 +0400

    add block validations

--- app/models/block.rb
+++ app/models/block.rb
@@ -3,6 +3,9 @@ class Block < ActiveRecord::Base
 
   attr_accessible :body, :on_main, :order_at, :state_event, :title
 
+  validates :title, presence: true
+  validates :body, presence: true
+
   state_machine :state, initial: :hidden do
     state :hidden
     state :published
