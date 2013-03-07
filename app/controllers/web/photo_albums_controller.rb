9937d84fa91af6ca8bf42f4cca5be5c22cb20296
Denis Starshenkov <starshenkov@bk.ru>
2013-03-07 09:57:02 +0000

    moving magick numbers into configus

--- app/controllers/web/photo_albums_controller.rb
+++ app/controllers/web/photo_albums_controller.rb
@@ -2,7 +2,7 @@ class Web::PhotoAlbumsController < Web::ApplicationController
   def index
     add_breadcrumb :home, :root_path
     title t (:photo_albums)
-    @photo_albums = PhotoAlbum.published.page(params[:page]).per(5).decorate
+    @photo_albums = PhotoAlbum.published.page(params[:page]).per(configus.per_page_count.photo_albums).decorate
 
     add_breadcrumb :photo_albums, photo_albums_path
   end
--- app/controllers/web/searches_controller.rb
+++ app/controllers/web/searches_controller.rb
@@ -2,6 +2,6 @@ class Web::SearchesController < Web::ApplicationController
   def show
     add_breadcrumb :search
     @term = params[:term]
-    @results = PgSearcher.search(@term).page(params[:page]).per(20)
+    @results = PgSearcher.search(@term).page(params[:page]).per(configus.per_page_count.searches)
   end
 end
