e13ad189f8807f3b7a51d336323530add4c9387a
Denis Starshenkov <starshenkov@bk.ru>
2013-03-11 14:47:31 +0000

    changed api controller

--- app/controllers/api/admin/study_requests_controller.rb
+++ app/controllers/api/admin/study_requests_controller.rb
@@ -1,9 +1,11 @@
 class Api::Admin::StudyRequestsController < Api::Admin::ApplicationController
+  respond_to :xlsx
+  respond_to :csv
 
   def index
     @q = StudyRequest.ransack(params[:q])
     @study_requests = @q.result
-    respond_to do |format|
+    respond_with (@study_requests) do |format|
       format.xlsx
       format.csv { render text: ::Converters::StudyRequestConverter.to_csv(@study_requests)}
     end
