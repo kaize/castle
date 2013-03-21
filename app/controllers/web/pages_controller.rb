b28065c27edb92fc83e9900cdc2630a3f1297b9e
login73ul <login73ul@gmail.com>
2013-03-21 17:30:40 +0400

    fixes sorting

--- app/controllers/web/pages_controller.rb
+++ app/controllers/web/pages_controller.rb
@@ -1,6 +1,6 @@
 class Web::PagesController < Web::ApplicationController
   def show
-    @page = Page.published.find params[:id]
+    @page = Page.web.find params[:id]
     add_breadcrumb @page.title, page_path
     title @page
   end
