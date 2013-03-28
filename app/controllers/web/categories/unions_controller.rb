0d02d2b7ffb97522fdd0a0d48fc08db5c7b647c4
login73ul <login73ul@gmail.com>
2013-03-28 12:07:36 +0400

    fixes 123, cange view schedule and union

--- app/controllers/web/categories/unions_controller.rb
+++ app/controllers/web/categories/unions_controller.rb
@@ -3,7 +3,6 @@ class Web::Categories::UnionsController < Web::ApplicationController
   def show
     @category = Category.find(params[:category_id])
     @union = @category.unions.find(params[:id])
-    add_breadcrumb @category.name, category_unions_path(@category)
     add_breadcrumb @union.name, category_union_path(@category, @union)
     title @category.name
     title @union.name
