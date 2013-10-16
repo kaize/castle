b9d6a102a4b863c914bb02b4b769948c460b0dfa
Anatoliy Pronin <piton.pronin@gmail.com>
2013-10-16 15:39:08 +0400

    fix welcome controller

--- app/controllers/web/welcome_controller.rb
+++ app/controllers/web/welcome_controller.rb
@@ -1,11 +1,11 @@
 class Web::WelcomeController < Web::ApplicationController
-  
+
   def index
     @news = News.web.limit(configus.limits.news)
-    @photo_album = PhotoAlbum.find_by_main(true).decorate
+    @photo_album = PhotoAlbumDecorator.find_by_main(true)
     @event = Event.find_by_main true
     @events = Event.scoped
     @blocks = Block.web
   end
-  
+
 end
