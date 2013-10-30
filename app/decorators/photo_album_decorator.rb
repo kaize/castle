22eb4751fb22325defd23a91db322c41e4c03dc9
Benjamin <ashanin.benjamin@gmail.com>
2013-10-30 18:00:36 +0400

    replace method from decorator to controller

--- app/decorators/photo_album_decorator.rb
+++ app/decorators/photo_album_decorator.rb
@@ -1,22 +1,3 @@
 class PhotoAlbumDecorator < ApplicationDecorator
   decorates :photo_album
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       source.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
-  def sorted_photos
-    photos.by_main
-  end
-
-  def main_photo
-    sorted_photos.first
-  end
-
-  def preview_photo(style = nil)
-    main_photo.image_url(style) if main_photo
-  end
 end
